2106e8f07d418ab7e0d9a304f61843a0aa4dbc6e
Xavier Lowmiller <xaver.lohmueller@me.com>
2020-03-06 10:01:39 +0100

    Solve Challenge 5

---
+++ SwiftUI Challenge/Challenge5.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+import CoreGraphics
+
+let p1 = CGPoint(x: 50, y: 50)
+let p2 = CGPoint(x: 100, y: 25)
+let p3 = CGPoint(x: 100, y: 100)
+
+struct Challenge5: View {
+	@State var toggle = true
+
+	var body: some View {
+		VStack {
+			Button("Toggle") {
+				withAnimation(.default) {
+					self.toggle.toggle()
+				}
+			}
+			AnimatedPath(from: toggle ? p1 : p2, to: p3)
+				.stroke(lineWidth: 2)
+		}
+	}
+}
+
+struct AnimatedPath: Shape {
+
+	var from: CGPoint
+	var to: CGPoint
+
+	func path(in rect: CGRect) -> Path {
+		Path { p in
+			p.move(to: from)
+			p.addLine(to: to)
+		}
+	}
+
+	var animatableData: AnimatablePair<CGPoint.AnimatableData, CGPoint.AnimatableData> {
+		get { AnimatablePair(AnimatablePair(from.x, from.y),
+												 AnimatablePair(to.x, to.y)) }
+		set {
+			(from.x, from.y) = (newValue.first.first, newValue.first.second)
+			(to.x, to.y) = (newValue.second.first, newValue.second.second)
+		}
+	}
+}
+
+struct Challenge5_Previews: PreviewProvider {
+	static var previews: some View {
+		Challenge5()
+	}
+}
